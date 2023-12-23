b0a4900c5fb276fdb52e2c223906fe1ca36c9cfb
pawel.pietocha <pawel.pietocha1991@gmail.com>
2023-12-23 10:11:41 +0100

    2 add sign up component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 export const routes: Routes = [
-
+{
+    path:'',
+    component: SignUpComponent
+}
 ];
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { CommonModule } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { SignUpForm } from './models/sign-up-form.model';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    MatCheckboxModule,
+    MatSelectModule, 
+    MatRadioModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  signUpForm = new FormGroup<SignUpForm>(new SignUpForm());
+  signUp = "Zarejestruj się";
+  login = "Zaloguj się";
+  maxDate = new Date();
+  voivodeships = [
+    "dolnośląskie",
+    "kujawsko-pomorskie",
+    "lubelskie",
+    "lubuskie",
+    "łódzkie",
+    "małopolskie",
+    "mazowieckie",
+    "opolskie",
+    "podkarpackie",
+    "podlaskie",
+    "pomorskie",
+    "śląskie",
+    "świętokrzyskie",
+    "warmińsko-mazurskie",
+    "wielkopolskie",
+    "zachodniopomorskie"
+    ]
 
+    ngOnInit(): void {
+      this.maxDate.setFullYear(this.maxDate.getFullYear() - 16);
+      this.signUpForm.controls.dateOfBorn.setValue(this.maxDate);
+    }
+    onSubmitSignUp(): any {
+      console.log(this.signUpForm);
+  }
 }
+
+
